7d8bef36bacd6f008b3f7c97bda46f917258a13b
anton.vaysberg <anton.vaysberg@servicenow.com>
2021-11-07 17:36:47 +0200

    unessecary imports removed

--- nftRating/rarityUpcoming.py
+++ nftRating/rarityUpcoming.py
@@ -1,11 +1,7 @@
-import re
 from time import sleep
-
 from bs4 import BeautifulSoup as bs
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import os
 
 options = Options()
 options.headless = True
@@ -15,15 +11,14 @@ options.add_argument("--window-size=1920,1200")
 DRIVER_PATH = './chromedriver'
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 driver.get("https://rarity.tools/upcoming")
+
+# Sleep needed to wait till page loaded
 sleep(5)
-# print(driver.page_source)
-# text = driver.page_source.split('\n')
 soup = bs(driver.page_source, 'html.parser')
 
 projects_full_list = []
 projects = soup.find_all('tr', class_="text-left text-gray-800")
 for project in projects:
-    # TODO project_name and project_description should be concatenated
     project_name = str(project.find('div', 'text-lg font-bold text-pink-700 dark:text-gray-300')).split('600px;">\n\t\t\t')[1].split('<!--')[0]
     project_description = str(project.find_all('div', attrs={'style':'max-width: 600px;'})[1]).split('600px;">\n\t\t\t\t')[1].split('\n\t\t\t</div')[0]
     for idx, a in enumerate(project.find_all('a', href=True)):
