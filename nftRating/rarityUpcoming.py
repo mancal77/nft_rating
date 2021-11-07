7ad4c06d242a545258be298160a5e3a397d069fc
anton.vaysberg <anton.vaysberg@servicenow.com>
2021-11-07 17:18:55 +0200

    project_name and project_description concatenation

--- nftRating/rarityUpcoming.py
+++ nftRating/rarityUpcoming.py
@@ -24,15 +24,15 @@ projects_full_list = []
 projects = soup.find_all('tr', class_="text-left text-gray-800")
 for project in projects:
     # TODO project_name and project_description should be concatenated
-    project_name = project.find('div', 'text-lg font-bold text-pink-700 dark:text-gray-300')
-    project_description = project.find_all('div', attrs={'style':'max-width: 600px;'})[1]
+    project_name = str(project.find('div', 'text-lg font-bold text-pink-700 dark:text-gray-300')).split('600px;">\n\t\t\t')[1].split('<!--')[0]
+    project_description = str(project.find_all('div', attrs={'style':'max-width: 600px;'})[1]).split('600px;">\n\t\t\t\t')[1].split('\n\t\t\t</div')[0]
     for idx, a in enumerate(project.find_all('a', href=True)):
         if idx == 0:
             discord = a['href']
         elif idx == 1:
             twitter = a['href']
-        else:
+        elif idx == 2:
             project_url = a['href']
-    projects_full_list.append((discord, twitter, project_url))
+    projects_full_list.append((project_name, project_description, discord, twitter, project_url))
 print(projects_full_list)
 driver.quit()
