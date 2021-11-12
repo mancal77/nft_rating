77c09faa572e885eed75df36b5dd500431b5f70c
anton.vaysberg <anton.vaysberg@servicenow.com>
2021-11-12 10:28:04 +0200

    insert to BigQuery

--- nftRating/bigQuery.py
+++ nftRating/bigQuery.py
@@ -1,71 +1,69 @@
 from google.cloud.exceptions import NotFound
 from google.cloud import bigquery
 
+client = bigquery.Client()
+client.project = 'disco-ascent-328216'
+dataset_id = "{}.nft_rating".format(client.project)
+table_id = '{}.nft_raw_data'.format(dataset_id)
 
-class bigQuery:
-    client = bigquery.Client()
-    client.project = 'disco-ascent-328216'
-    dataset_id = "{}.nft_rating".format(client.project)
-    table_id = '{}.nft_raw_data'.format(dataset_id)
 
-    def __init__(self):
-        # self.client = bigquery.Client()
-        self.client.project = client.project
-        self.dataset_id = dataset_id
-        self.table_id = table_id
+def create_data_set():
+    # Construct a full Dataset object to send to the API.
+    dataset = bigquery.Dataset(dataset_id)
+    # Geographic location where the dataset should reside.
+    dataset.location = "US"
+    # Send the dataset to the API for creation, with an explicit timeout.
+    dataset = client.create_dataset(dataset, timeout=30)  # Make an API request.
+    print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
 
-    def create_data_set(self):
-        # Construct a full Dataset object to send to the API.
-        dataset = bigquery.Dataset(self.dataset_id)
-        # Geographic location where the dataset should reside.
-        dataset.location = "US"
-        # Send the dataset to the API for creation, with an explicit timeout.
-        dataset = self.client.create_dataset(dataset, timeout=30)  # Make an API request.
-        print("Created dataset {}.{}".format(self.client.project, dataset.dataset_id))
 
-    def create_table(self):
-        schema = [
-            bigquery.SchemaField("project_name", "STRING", mode="REQUIRED"),
-            bigquery.SchemaField("project_description", "STRING"),
-            bigquery.SchemaField("discord", "STRING"),
-            bigquery.SchemaField("twitter", "STRING"),
-            bigquery.SchemaField("project_url", "STRING"),
-            bigquery.SchemaField("project_reg_date", "DATE"),
-            bigquery.SchemaField("twitter_reg_date", "DATE"),
-            bigquery.SchemaField("new_twits", "INTEGER"),
-            bigquery.SchemaField("twitter_followers", "INTEGER"),
-            bigquery.SchemaField("twitter_popular_followers", "INTEGER"),
-            bigquery.SchemaField("twitter_mentions", "INTEGER"),
-            bigquery.SchemaField("additional_functionality", "STRING")
-        ]
+def create_table():
+    schema = [
+        bigquery.SchemaField("item_name", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("item_type", "STRING", mode="REQUIRED"),  # 1-project, 2-artwork, 3-collection, 4-creator
+        bigquery.SchemaField("item_token", "STRING"),
+        bigquery.SchemaField("project_description", "STRING"),
+        bigquery.SchemaField("twitter", "STRING"),
+        bigquery.SchemaField("project_url", "STRING"),
+        bigquery.SchemaField("discord", "STRING"),
+        bigquery.SchemaField("project_reg_date", "DATE"),
+        bigquery.SchemaField("twitter_reg_date", "DATE"),
+        bigquery.SchemaField("new_twits", "INTEGER"),
+        bigquery.SchemaField("twitter_followers", "INTEGER"),
+        bigquery.SchemaField("twitter_popular_followers", "INTEGER"),
+        bigquery.SchemaField("twitter_mentions", "INTEGER"),
+        bigquery.SchemaField("additional_functionality", "STRING")
+    ]
 
-        table = bigquery.Table(self.table_id, schema=schema)
-        table = self.client.create_table(table)  # Make an API request.
-        print(
-            "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
-        )
+    table = bigquery.Table(table_id, schema=schema)
+    table = client.create_table(table)  # Make an API request.
+    print(
+        "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+    )
 
-    def insert_rows(self, rows_to_insert):
-        errors = self.client.insert_rows_json(self.table_id, rows_to_insert)  # Make an API request.
-        if not errors:
-            print("New rows have been added.")
-        else:
-            print("Encountered errors while inserting rows: {}".format(errors))
 
-    # Check if Data Set exists and if not create it
-    try:
-        client.get_dataset(dataset_id)  # Make an API request.
-        print("Dataset {} already exists".format(dataset_id))
-    except NotFound:
-        print("Dataset {} is not found".format(dataset_id))
-        print("Creating dataset {}".format(dataset_id))
-        create_data_set()
+def insert_rows(rows_to_insert):
+    errors = client.insert_rows_json(table_id, rows_to_insert)  # Make an API request.
+    if not errors:
+        print("New rows have been added.")
+    else:
+        print("Encountered errors while inserting rows: {}".format(errors))
 
-    # Check if Table exists and if not create it
-    try:
-        client.get_table(table_id)  # Make an API request.
-        print("Table {} already exists.".format(table_id))
-    except NotFound:
-        print("Table {} is not found.".format(table_id))
-        print("Creating table {}".format(table_id))
-        create_table()
+
+# Check if Data Set exists and if not create it
+try:
+    client.get_dataset(dataset_id)  # Make an API request.
+    print("Dataset {} already exists".format(dataset_id))
+except NotFound:
+    print("Dataset {} is not found".format(dataset_id))
+    print("Creating dataset {}".format(dataset_id))
+    create_data_set()
+
+# Check if Table exists and if not create it
+try:
+    client.get_table(table_id)  # Make an API request.
+    print("Table {} already exists.".format(table_id))
+except NotFound:
+    print("Table {} is not found.".format(table_id))
+    print("Creating table {}".format(table_id))
+    create_table()
--- nftRating/rarityUpcoming.py
+++ nftRating/rarityUpcoming.py
@@ -3,12 +3,11 @@ from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from bigQuery import bigQuery
+from bigQuery import insert_rows
 
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
-
 
 DRIVER_PATH = './chromedriver'
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
@@ -19,18 +18,27 @@ sleep(5)
 soup = bs(driver.page_source, 'html.parser')
 
 projects_full_list = []
+twitter = None
+project_url = None
+discord = None
 projects = soup.find_all('tr', class_="text-left text-gray-800")
 for project in projects:
-    project_name = str(project.find('div', 'text-lg font-bold text-pink-700 dark:text-gray-300')).split('600px;">\n\t\t\t')[1].split('<!--')[0]
-    project_description = str(project.find_all('div', attrs={'style':'max-width: 600px;'})[1]).split('600px;">\n\t\t\t\t')[1].split('\n\t\t\t</div')[0]
+    projects_full = {}
+    project_name = str(project.find('div', 'text-lg font-bold text-pink-700 dark:text-gray-300')).split('600px;">\n\t\t\t')[1].split(
+        '<!--')[0]
+    project_description = str(project.find_all('div', attrs={'style': 'max-width: 600px;'})[1]).split('600px;">\n\t\t\t\t')[1].split(
+        '\n\t\t\t</div')[0]
     for idx, a in enumerate(project.find_all('a', href=True)):
-        if idx == 0:
+        if str(a['href']).__contains__('discord'):
             discord = a['href']
-        elif idx == 1:
+        elif str(a['href']).__contains__('twitter'):
             twitter = a['href']
-        elif idx == 2:
+        else:
             project_url = a['href']
-    projects_full_list.append((project_name, project_description, discord, twitter, project_url))
+    projects_full.update(
+        {"item_name": project_name, "item_type": 1, "project_description": project_description, "twitter": twitter,
+         "project_url": project_url, "discord": discord})
+    projects_full_list.append(projects_full)
 print(projects_full_list)
 driver.quit()
-bigQuery.insert_rows(projects_full_list)
+insert_rows(projects_full_list)
