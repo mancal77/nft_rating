5983c1f98e7d08387afb3253453b8297a1bf2878
anton.vaysberg <anton.vaysberg@servicenow.com>
2021-12-12 21:28:46 +0200

    calculation 0 days fix

--- nftRating/calcRating.py
+++ nftRating/calcRating.py
@@ -3,6 +3,8 @@ from datetime import date
 
 def calculate_rating(twitter_reg_date, followers, statuses_since_reg, avg_twits_sentiment, total_tweets, total_tweets_7days):
     days_since_reg = (date.today() - twitter_reg_date).days
+    if days_since_reg == 0:
+        days_since_reg = 1
 
     return followers * 0.2 / days_since_reg + statuses_since_reg * 0.2 / days_since_reg + \
            avg_twits_sentiment * 0.3 + total_tweets * 0.1 / days_since_reg + \
